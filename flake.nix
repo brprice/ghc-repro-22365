3d8eceda1667abc65af7e5c6f270c275c6303b11
Ben Price <ben@hackworthltd.com>
2023-03-15 16:06:30 +0000

    add `nix run` script

--- flake.nix
+++ flake.nix
@@ -3,9 +3,29 @@
 
   outputs = { self, nixpkgs }: let
     pkgs = import nixpkgs {system = "x86_64-linux";};
+    deps = [pkgs.cabal-install (pkgs.haskell.packages.ghc902.ghcWithPackages (ps: [ps.async]))];
+    runScript = pkgs.writeShellApplication {
+        name = "bugloop";
+        runtimeInputs = deps;
+        text = ''
+          cabal clean
+          cabal build bug
+          bug=$(cabal list-bin bug)
+          i=1
+          while :; do
+            echo $i
+            $bug
+            ((i++))
+          done
+        '';
+        };
   in {
     devShell.x86_64-linux = pkgs.mkShell {
-      packages = [pkgs.cabal-install (pkgs.haskell.packages.ghc902.ghcWithPackages (ps: [ps.async]))];
+      packages = deps;
     };
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${runScript}/bin/bugloop" ;
+      };
   };
 }
